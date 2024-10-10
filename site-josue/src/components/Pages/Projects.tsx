7281e2f83553fb733fa231eb0fbe1e76fbad13ba
Filipe Sanches <53585862+filipesanches@users.noreply.github.com>
2024-10-10 14:13:08 +0000

    ok

--- site-josue/src/components/Pages/Projects.tsx
+++ site-josue/src/components/Pages/Projects.tsx
@@ -1,14 +1,37 @@
-import { Box, Typography } from '@mui/material';
+import { Box, List, ListItem, ListItemIcon, ListItemText, Typography } from '@mui/material';
 import Pagination from '../Pagination';
 import Carousel from '../Carousel';
+import FiberManualRecordIcon from '@mui/icons-material/FiberManualRecord';
+
+
+const DynamicList = ({ items }) => {
+  return (
+    <List component="ol" disablePadding>
+      {items.map((item, index) => (
+        <ListItem key={index} component="li" disablePadding>
+          <ListItemIcon sx={{ minWidth: '30px' }}>
+            <FiberManualRecordIcon fontSize="inherit" />
+          </ListItemIcon>
+          <ListItemText primary={item.primary} />
+        </ListItem>
+      ))}
+    </List>
+  );
+};
 
 const BoxProjects = ({ images }: { images: string[] }) => {
   return (
-    <Box component="div" sx={{ display: 'flex', flexFlow: {md: 'column nowrap', lg: 'row nowrap'} }}>
+    <Box
+      component="div"
+      sx={{
+        display: 'flex',
+        flexFlow: { xs: 'column nowrap', lg: 'row nowrap' },
+      }}
+    >
       <Box sx={{ marginRight: { md: 0, lg: '60px' } }}>
         <Carousel images={images} />
       </Box>
-      <Box sx={{ textAlign: { xs: 'center', md: 'left' } }}>
+      <Box sx={{ textAlign: { xs: 'center', lg: 'left' } }}>
         <Typography variant="h5" component="div">
           Amazon Fulfillment Office and Restroom
         </Typography>
@@ -16,16 +39,15 @@ const BoxProjects = ({ images }: { images: string[] }) => {
           variant="body1"
           component="div"
           sx={{
-            fontWeight: 'bold',
             color: 'gray',
             position: 'relative',
-            marginBottom: '50px',
+            marginBottom: '30px',
             paddingBottom: '20px',
-            textAlign: { xs: 'center', md: 'left' },
+            textAlign: { xs: 'center', lg: 'left' },
             '::after': {
               content: '""',
               display: 'block',
-              width: '300px',
+              width: { xs: '100%', lg: '250px' },
               borderBottom: '4px solid #7ebec5',
               position: 'absolute',
               bottom: 0,
@@ -36,6 +58,13 @@ const BoxProjects = ({ images }: { images: string[] }) => {
           {' '}
           Twsad
         </Typography>
+        <DynamicList
+          items={[
+            { primary: 'Project Name', secondary: 'Project Name' },
+            { primary: 'Project Name', secondary: 'Project Name' },
+            { primary: 'Project Name', secondary: 'Project Name' },
+          ]}
+        />
       </Box>
     </Box>
   );
